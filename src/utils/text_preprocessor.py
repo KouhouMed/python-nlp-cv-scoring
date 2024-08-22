de5905b6eee5c1dd0d1095516784e9bf72fadec4
KouhouMed <mohamedkouhou@gmail.com>
2024-08-22 23:59:57 +0200

    text preprocessor

--- src/utils/text_preprocessor.py
+++ src/utils/text_preprocessor.py
@@ -10,3 +10,21 @@ class TextPreprocessor:
         self.language = language
         self.stop_words = set(stopwords.words(self.language))
         self.lemmatizer = WordNetLemmatizer()
+
+        def preprocess(self, text):
+            # Convert to lowercase
+            text = text.lower()
+
+            # Remove special characters and digits
+            text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+            # Tokenize
+            tokens = word_tokenize(text)
+
+            # Remove stopwords and lemmatize
+            cleaned_tokens = [self.lemmatizer.lemmatize(token) for token in tokens if token not in self.stop_words]
+
+            # Join tokens back into string
+            cleaned_text = ' '.join(cleaned_tokens)
+
+            return cleaned_text
