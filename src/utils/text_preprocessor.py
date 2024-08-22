1ff753ce4b930883a5dbbd30f61ee37c1c9e7eb9
KouhouMed <mohamedkouhou@gmail.com>
2024-08-22 23:59:40 +0200

    text preprocessor

--- src/utils/text_preprocessor.py
+++ src/utils/text_preprocessor.py
@@ -3,3 +3,10 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+
+
+class TextPreprocessor:
+    def __init__(self, language='french'):
+        self.language = language
+        self.stop_words = set(stopwords.words(self.language))
+        self.lemmatizer = WordNetLemmatizer()
